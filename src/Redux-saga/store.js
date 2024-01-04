c6ff8d5e31dffcb6ce2a59ce0cb1a41dc18c126a
divkakadiya <divkakadiyarnw@gmail.com>
2024-01-04 14:09:18 +0530

    E-election

---
+++ src/Redux-saga/store.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from '@redux-saga/core'
+import RootReducer from './rootreduser';
+import { SagaIndex } from '../Redux-saga/Admin_Code/Saga/index';
+import { voteSaga } from './User_code/saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const SagaMiddleware = createSagaMiddleware()
+
+const store = createStore(
+  RootReducer,
+  composeWithDevTools(applyMiddleware(SagaMiddleware))
+)
+SagaMiddleware.run(SagaIndex);
+SagaMiddleware.run(voteSaga)
+
+export default store;
